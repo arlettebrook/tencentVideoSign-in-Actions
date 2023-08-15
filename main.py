9c4148b6afb682927748c6516f3136fb01cca2ee
andrewbrook <andrewbrook@qq.com>
2023-08-15 17:03:31 +0800

    add iqiyi task

--- main.py
+++ main.py
@@ -6,24 +6,34 @@ from loguru import logger
 from config import TencentVideo, IQY
 
 
+def run_tvd():
+    tencentVideo = TencentVideo()
+    logger.success("腾讯视频自动签到启动成功")
+    tencentVideo.tencent_video_sign_in()
+    logger.success("腾讯视频任务已完成")
+
+
+def run_aqy():
+    iqy = IQY()
+    logger.success("爱奇艺任务启动成功")
+    iqy.check_in()
+    time.sleep(3)
+    iqy.get_rewards()
+    time.sleep(10)
+    iqy.get_user_info()
+    logger.success("爱奇艺任务已完成")
+
+
 def main():
     try:
         logger.success("autoCheck-in启动成功")
         if os.getenv('LOGIN_COOKIE'):
-            tencentVideo = TencentVideo()
-            logger.success("腾讯视频自动签到启动成功")
-            tencentVideo.tencent_video_sign_in()
-            logger.success("腾讯视频任务已完成")
+            run_tvd()
         if os.getenv("IQY_COOKIE"):
-            iqy = IQY()
-            logger.success("爱奇艺任务启动成功")
-            iqy.get_rewards()
-            time.sleep(3)
-            iqy.get_user_info()
-            logger.success("爱奇艺任务已完成")
+            run_aqy()
         logger.info("所有任务已完成")
-        logger.success("10秒之后退出程序")
-        time.sleep(10)
+        logger.success("5秒之后退出程序")
+        time.sleep(5)
     except Exception as e:
         logger.error(e)
 
